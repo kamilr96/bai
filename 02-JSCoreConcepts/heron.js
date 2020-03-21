e41917829e340e21e8994e2eef413e7660319e51
Kamil Rąpała <rapala.kamil@gmail.com>
2020-03-21 20:29:50 +0100

    Dodano wzór herona

--- 02-JSCoreConcepts/heron.js
+++ 02-JSCoreConcepts/heron.js
@@ -5,7 +5,8 @@ c = 5;
 h = 7
 
 // Pole trókąta o bokach ..., ... oraz ... wynosi ... .
-
-let a = () => "aaaa";
+p = (b+c+h)/2;
+pole_heron = Math.sqrt(p*(p-b)*(p-c)*(p-h));
+let a = () => `Pole trójkąta o bokach ${b}, ${c} oraz ${h} wynosi ${pole_heron}.`;
 
 console.log( a() );
